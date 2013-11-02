d4dedef3bd0224994f372ac593f07a4e3d209465
André Erdmann <dywi@mailerd.de>
2013-11-02 21:02:50 +0100

    shlibcclib/message.py: add force_debug_print()

--- shlibcclib/message.py
+++ shlibcclib/message.py
@@ -2,9 +2,12 @@ import sys
 
 DEBUG_PRINT = True
 
-def debug_print ( _str ):
-   if DEBUG_PRINT:
+def debug_print ( _str, force_print=False ):
+   if force_print or DEBUG_PRINT:
       sys.stderr.flush()
       sys.stderr.write ( "debug: " )
       sys.stderr.write ( _str )
       sys.stderr.write ( "\n" )
+
+def force_debug_print ( _str ):
+   debug_print ( _str, True )
